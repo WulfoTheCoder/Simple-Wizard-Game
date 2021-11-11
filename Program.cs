6b7ad467c50a5137f3de966d42021e93dee12ed8
WulfoTheCoder <noobplayingvideogames14@gmail.com>
2021-11-11 23:08:08 +0100

    update v2.0

--- Program.cs
+++ Program.cs
@@ -4,26 +4,60 @@ namespace WizardSimulator {
     class Program {
         static void Main(string[] args) {
             try {
-                // Addany spell you like
-                Spell abrakadabra = new Spell("Avada Kedavra", "damage", 100f);
-                Spell unexpectoPatronum = new Spell("Unexpecto Patronum", "damage", 10f);
-                Spell reparo = new Spell("Reparo", "misc", 0f);
-                Spell someHealingSpell = new Spell("Some Healing Spell", "healing", 10f);
+                string command = "";
 
-                // Create custom wizards with the list of available spells for them
-                Wizard wizard01 = new Wizard("Parry Hopper", new List<Spell>{abrakadabra, unexpectoPatronum, reparo, someHealingSpell}, 4);
-                Wizard wizard02 = new Wizard("Marry Jopper", new List<Spell>{someHealingSpell, unexpectoPatronum}, 2);
+                List<Spell> spells = new List<Spell>();
+                List<Wizard> wizards = new List<Wizard>();
+
+                while(command != "break") {
+                    command = Console.ReadLine();
+                    string[] toks = command.Split(' ');
+                    
+                    if(toks[0] == "spell-new") {
+                        if(toks.Length != 4) {
+                            throw new Exception("Invalid number of arguments! spell-new command should have 3 arguments after it!");
+                        }
+                        Spell generatedSpell = new Spell(toks[1].Replace('_', ' '), toks[2], float.Parse(toks[3]));
+                        spells.Add(generatedSpell);
+                    }
+
+                    if(toks[0] == "wizard-new") {
+                        if(toks.Length != 4) throw new Exception("Invalid number of arguments! wizard-new command should have 3 arguments after it!");
+                        string[] spellNames = toks[2].Split(',');
+                        List<Spell> generatedSpells = new List<Spell>();
+
+                        foreach(string spellName in spellNames) {
+                            foreach(Spell spell in spells) {
+                                if(spellName.Replace('_', ' ') == spell.name) {
+                                    generatedSpells.Add(spell);
+                                }
+                            }
+                        }
+
+                        if(generatedSpells.Count <= 0) {
+                            throw new Exception("No assigned spells to this wizard");
+                        }
+
+                        Wizard generatedWizard = new Wizard(toks[1].Replace('_', ' '), generatedSpells, int.Parse(toks[3]));
+                        wizards.Add(generatedWizard);
+                    }
+                }
+
+                Console.WriteLine("\nSpells generated:");
+                foreach(Spell spell in spells) {
+                    Console.WriteLine("\t" + spell.name);
+                }
                 
-                // Make them fight
-                wizard01.Attack(wizard02, unexpectoPatronum);
-                wizard02.CastSpell(someHealingSpell);
-                wizard02.Attack(wizard01, unexpectoPatronum);
-                wizard02.Attack(wizard01, unexpectoPatronum);
-                wizard02.Meditate();
-                wizard02.GainSlot();
-                wizard01.Attack(wizard02, unexpectoPatronum);
-                wizard02.CastSpell(someHealingSpell);
-                wizard01.Attack(wizard02, unexpectoPatronum);
+                Console.WriteLine("\nWizards generated:");
+                foreach(Wizard wiz in wizards) {
+                    Console.WriteLine("\t" + wiz.name);
+                }
+                
+                // GENERAL INPUT
+                // spell-new Adava_Kedabra damage 100
+                // wizard-new Merry_Poppins Adava_Kedabra 1
+                // break
+
             } catch(Exception e) {
                 Console.WriteLine(e.Message);
             }
